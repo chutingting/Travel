6032681dcddf5f208108ffb157d413e86e2bf1a1
chutingting <chutingting@9fbank.com.cn>
2019-01-04 16:07:05 +0800

    add header

---
+++ src/main.js
@@ -0,0 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+/* 解决移动端 300毫秒 点击延迟问题 */
+import fastClick from 'fastclick'
+import 'styles/reset.css'
+/* 解决1像素问题 */
+import 'styles/border.css'
+import 'styles/iconfont.css'
+
+Vue.config.productionTip = false
+fastClick.attach(document.body)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/pages/home/components/header.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+            <div class="icon back-icon">&#xe624;</div>
+        </div>
+        <div class="header-input">
+            <span class="icon">&#xe632;</span>输入城市/景点/游玩主题</div>
+        <div class="header-right">城市
+            <span class="icon icon-jiantou"></span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+<style scoped lang="stylus">
+@import '~styles/varibles.styl';
+
+.header {
+    height: 0.86rem;
+    line-height: 0.86rem;
+    display: flex;
+    background: $bgColor;
+    color: #fff;
+
+    .header-left {
+        width: 0.64rem;
+        float: left;
+
+        .back-icon {
+            font-size: 0.4rem;
+            text-align: center;
+        }
+    }
+
+    .header-input {
+        flex: 1;
+        background: #fff;
+        margin-top: 0.12rem;
+        margin-left: 0.2rem;
+        padding-left: 0.2rem;
+        height: 0.64rem;
+        line-height: 0.64rem;
+        border-radius: 0.1rem;
+        color: #ccc;
+    }
+
+    .header-right {
+        width: 1.24rem;
+        float: right;
+        text-align: center;
+    }
+}
+</style>
