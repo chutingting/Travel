c0a3f709622fbdff76bfc5a4672fb77b2a33378d
chutingting <chutingting@9fbank.com.cn>
2019-01-10 15:53:42 +0800

    增加详情页面header渐隐渐现效果

--- src/pages/detail/components/header.vue
+++ src/pages/detail/components/header.vue
@@ -1,22 +1,87 @@
 <template>
     <div>
-        <div class="header-abs">
-        </div>
-        <div class="header-fixed">
+        <router-link tag="div" to="/" class="header-abs" v-show="showAbs">
+            <div class="icon header-abs-back">&#xe624;</div>
+        </router-link>
+        <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+            <router-link to="/">
+                <div class="icon header-fixed-back">&#xe624;</div>
+            </router-link>
+            景点详情
         </div>
     </div>
 </template>
 <script>
 export default {
   name: 'DetailHeader',
-  components: {
-    CommonGallary
-  },
   data() {
-    return {}
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
   },
-  methods: {}
+  activated() {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    handleScroll() {
+      const top = document.documentElement.scrollTop
+      if (top > 60) {
+        let opacity = top / 140
+        opacity = opacity > 1 ? 1 : opacity
+        this.opacityStyle = {
+          opacity: opacity
+        }
+        this.showAbs = false
+      } else {
+        this.showAbs = true
+      }
+    }
+  }
 }
 </script>
 <style scoped lang='stylus'>
+@import '~styles/varibles.styl';
+
+.header-abs {
+    position: absolute;
+    left: 0.2rem;
+    top: 0.2rem;
+    width: 0.8rem;
+    height: 0.8rem;
+    border-radius: 0.4rem;
+    background: rgba(0, 0, 0, 0.8);
+    text-align: center;
+    line-height: 0.8rem;
+
+    .header-abs-back {
+        color: #ffffff;
+        font-size: 0.4rem;
+    }
+}
+
+.header-fixed {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: $headerHeight;
+    line-height: $headerHeight;
+    text-align: center;
+    color: #fff;
+    background: $bgColor;
+    font-size: 0.32rem;
+
+    .header-fixed-back {
+        width: 0.64rem;
+        font: size 0.4rem;
+        text-align: center;
+        position: absolute;
+        top: 0;
+        left: 0;
+        color: #fff;
+    }
+}
 </style>
